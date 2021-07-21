1a509c357aedc28534162731c373592ac56e5c9b
Tim <tnematjonov@mail.ru>
2021-07-21 11:21:04 +0500

    end

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let Num = +prompt('Введите число: ');
 let Pow = +prompt('Введите степень: ');
 let Result = Num;
 
-for (let i = 1; i < pow; i++) {
+for (let i = 1; i < Pow; i++) {
     Result *= Num;  
 }
 alert('Ответ: ' + Result);
